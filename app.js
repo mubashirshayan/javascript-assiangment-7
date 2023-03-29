39d9e314faff0e57453654ec9297aaf04576bb4e
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-03-29 13:55:10 +0500

    new files

---
+++ app.js
@@ -0,0 +1,54 @@
+//Question no1
+/*var inpt=prompt('enter positive integer')
+document.write('number:'+inpt+'<br>');
+document.write('round of value:'+Math.round(inpt)+'<br>');
+document.write('floor value:'+Math.floor(inpt)+'<br>');
+document.write('ceil value'+Math.ceil(inpt));*/
+//Question no2
+/*var inpt=prompt('enter negative floating point')
+document.write('number:'+inpt+'<br>');
+document.write('round of value:'+Math.round(inpt)+'<br>');
+document.write('floor value:'+Math.floor(inpt)+'<br>');
+document.write('ceil value'+Math.ceil(inpt));*/
+//Question no3
+/*var user=-6
+document.write(Math.abs(user));*/
+//Question no4
+/*var disc=Math.random(1)*6;
+var rand=Math.round(disc);
+document.write('random dise value:'+rand);*/
+//Question no 5
+/*var ranNum=Math.random()*1;
+var abs=Math.round(ranNum);
+if (abs==0) {
+  document.write(abs+'random coin value:Heads');
+} else if(abs==1){
+  document.write(abs+'random coin value:Tails');
+}*/
+
+//Question no6
+/*var rand=Math.random(1)*100;
+var num=Math.round(rand);
+document.write('random number between 1 and 100:'+num)*/
+//Question no7
+/*var ask=prompt('Enter your weight in kilograms')
+var askLen=ask.length;
+var weight=''
+console.log(askLen);
+for (var i=0; i<askLen;i++) {
+  if (i<4) {
+    weight+=ask[i]
+  }
+}
+document.write('The weight of user is'+weight+' '+'kilograms')*/
+//Question no8
+/*var ask=prompt('enter a number between 1 and 10')
+var rand=Math.random(1)*10;
+var num=Math.round(rand);
+console.log(num)
+if (ask==num) {
+  alert('congrlations');
+}else{
+  alert('Try again!')
+}
+*/
